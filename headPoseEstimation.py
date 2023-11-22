65740b0f22a4bfcaa1954efb37e27207359374f4
mmhungry <105326726+mmhungry@users.noreply.github.com>
2023-11-22 21:59:05 +0700

    real-time cheating detection

--- headPoseEstimation.py
+++ headPoseEstimation.py
@@ -2,6 +2,12 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import time
+from tensorflow.keras.models import load_model
+
+# Load your cheating detection model
+cheating_detection_model = load_model('BehaviorClassification_90%.h5')
+# Buffer to store time steps
+time_steps_buffer = []
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -11,6 +17,21 @@ mp_drawing = mp.solutions.drawing_utils
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 cap = cv2.VideoCapture(0)
+
+def compute_features_based_on_head_pose(image, face_mesh):
+    frame_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(frame_rgb)
+    
+    # Extract and flatten face landmarks
+    keypoints = extract_face_keypoints(results)
+    return keypoints
+
+def extract_face_keypoints(results):
+    if results.multi_face_landmarks:
+        face = [[lm.x, lm.y, lm.z] for lm in results.multi_face_landmarks[0].landmark]
+        return np.array(face).flatten()
+    else:
+        return np.zeros(468 * 3)  # Assuming 468 landmarks, each with x, y, z
 
 if not cap.isOpened():
     print("Error: Camera not found or could not be opened.")
@@ -22,7 +43,14 @@ while cap.isOpened():
     if not success:
         print("Error reading frame from camera.")
         continue
+    # Compute the features for the current frame based on head pose
+    current_features = compute_features_based_on_head_pose(image, face_mesh)
+    time_steps_buffer.append(current_features)
 
+    if len(time_steps_buffer) > 30:
+        time_steps_buffer.pop(0)
+
+    # Head Pose Estimation 
     start = time.time()
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 
@@ -79,16 +107,16 @@ while cap.isOpened():
             z = angles[2] * 360
 
             #see where the user's head tilting
-            if y < -5:
-                text = "Looking Left"
-            elif y > 5:
-                text = "Looking Right"
-            elif x < -3:
-                text = "Looking Down"
-            elif x > 5:
-                text = "Looking Up"
-            else:
-                text = "Forward"
+            # if y < -5:
+            #     text = "Looking Left"
+            # elif y > 5:
+            #     text = "Looking Right"
+            # elif x < -3:
+            #     text = "Looking Down"
+            # elif x > 5:
+            #     text = "Looking Up"
+            # else:
+            #     text = "Forward"
             
             #display the nose direction
             nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
@@ -99,7 +127,7 @@ while cap.isOpened():
             cv2.line(image, p1, p2, (255, 0, 0), 3)
 
             #Add text on the image
-            cv2.putText(image, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+            # cv2.putText(image, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
             cv2.putText(image, "x: "+str(np.round(x, 2)), (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
             cv2.putText(image, "y: "+str(np.round(y, 2)), (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
             cv2.putText(image, "z: "+str(np.round(z, 2)), (500, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
@@ -118,8 +146,33 @@ while cap.isOpened():
             landmark_drawing_spec = drawing_spec,
             connection_drawing_spec = drawing_spec
         )
-    cv2.imshow('Head Pose Estimation', image)
 
+
+    # When buffer has 30 time steps, make a prediction
+    if len(time_steps_buffer) == 30:
+        model_input = np.array(time_steps_buffer).reshape(1, 30, 1404)
+        cheating_prediction = cheating_detection_model.predict(model_input)
+
+        # Correctly process the cheating_prediction
+        predicted_class = np.argmax(cheating_prediction, axis=1)[0]  # Get the predicted class index
+        cheating_type = ""  # Initialize an empty string for cheating type
+
+        if predicted_class == 1:
+            cheating_text = "Cheating Type 1"
+            cheating_type = "Type 1"
+        elif predicted_class == 2:
+            cheating_text = "Cheating Type 2"
+            cheating_type = "Type 2"
+        else:
+            cheating_text = "Not Cheating"
+        # Display the cheating detection result
+        cv2.putText(image, cheating_text, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if cheating_type:
+            cv2.putText(image, cheating_type, (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+        
+    cv2.imshow('Head Pose Estimation', image)
     if cv2.waitKey(5) & 0xFF == 27:
         break
 
